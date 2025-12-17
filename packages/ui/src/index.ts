c695cd153173da276eaecd678bbdf7da10043b70
RizwanSuvo99 <rizwanshuvo@gmail.com>
2025-12-17 11:30:00 +0600

    📦 feat(ui): add Button component with variants

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -5,8 +5,6 @@
 // Utilities
 export { cn } from './lib/utils';
 
-// Components will be added as they are built:
-// export { Button } from './components/button';
-// export { Input } from './components/input';
-// export { Badge } from './components/badge';
-// ... etc.
+// Components
+export { Button, buttonVariants } from './components/button';
+export type { ButtonProps } from './components/button';
