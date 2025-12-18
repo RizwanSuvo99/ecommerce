8c0e2b38c050532a5e05acef0889d1af70ff9af8
RizwanSuvo99 <rizwanshuvo@gmail.com>
2025-12-18 10:45:12 +0600

    🔧 fix(api): register InvoiceService in OrdersModule providers

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 
+import { InvoiceService } from './invoice.service';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { ShippingService } from './shipping.service';
@@ -7,7 +8,7 @@ import { TaxService } from './tax.service';
 
 @Module({
   controllers: [OrdersController],
-  providers: [OrdersService, ShippingService, TaxService],
+  providers: [OrdersService, ShippingService, TaxService, InvoiceService],
   exports: [OrdersService, ShippingService, TaxService],
 })
 export class OrdersModule {}
